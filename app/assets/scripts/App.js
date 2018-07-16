ca6e6003205bd7b80f0a4df3a28531386ca9c914
Tom Vancev <tome_va@hotmail.com>
2018-07-16 15:33:49 +0200

    Completed the modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,11 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
-import StickyHeader from './modules/StickyHeader';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu(); 
 new RevealOnScroll($(".feature-item"),"85%");
 new RevealOnScroll($(".testemonial"),"60%");
 var stickyHeader = new StickyHeader(); 
+var modal = new Modal(); 
